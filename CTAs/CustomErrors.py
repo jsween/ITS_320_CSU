39328262a6356af22a7e7c84a8f8b62ee310ffad
Jon Sweeney <jonsweeney26@gmail.com>
2024-05-02 15:33:44 -0700

    docs ;

--- CTAs/CustomErrors.py
+++ CTAs/CustomErrors.py
@@ -1,10 +1,16 @@
 class TooManyAttemptsError(Exception):
+    """
+    Raised when a user attempts to run out of attempts.
+    """
     def __init__(self, message="Too many failed attempts."):
         self.message = message
         super().__init__(self.message)
 
 
 class InvalidNegativeNumberError(Exception):
+    """
+    Raised when a user attempts to enter a negative number when not allowed.
+    """
     def __init__(self, message="Invalid negative number."):
         self.message = message
         super().__init__(self.message)
